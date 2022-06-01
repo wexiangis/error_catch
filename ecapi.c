66a52cdf13b5ab39af8856ab281e9963f28db6df
wexiangis <wexiangis@qq.com>
2022-06-01 16:44:30 +0800

    [vdso]支持

--- ecapi.c
+++ ecapi.c
@@ -39,13 +39,22 @@ typedef struct {
     void* dynstr;
 } ECElf_Struct;
 
-static void ecelf_release(ECElf_Struct* elf);
-static int ecelf_load(const char* file, ECElf_Struct* elf);
-static int ecapi_loadSymbolFromElfByAddr(ECElf_Struct* elf, uint64_t addr, char* symbol, int symbolLen);
-static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, int positionLen);
-
 static ECAPI_CALLBACK g_ecapi_callback = NULL;
 static void* g_ecapi_priv = NULL;
+
+static void ecelf_release(ECElf_Struct* elf);
+static int ecelf_load(const char* file, ECElf_Struct* elf);
+static int ecelf_loadByMem(uint8_t* addr, ECElf_Struct* elf);
+static int ecapi_loadSymbolFromElfByAddr(ECElf_Struct* elf, uint64_t addr, char* symbol, int symbolLen);
+static int ecapi_parse_proc_self_maps(uint64_t* addrList, int addrListLen, char** positionList, int positionLen);
+
+static int _isSameLevel(uint64_t v1, uint64_t v2)
+{
+    uint64_t max = v1 > v2 ? v1 : v2;
+    if (((v1 & v2) << 1) > max)
+        return 1;
+    return false;
+}
 
 static void ecapi_signal(int sig)
 {
@@ -66,24 +75,25 @@ static void ecapi_signal(int sig)
             // printf("%dth %08llX \r\n", i, (long long unsigned int)addrList[i]);
         }
         /* 指针定点 */
-        for (i = 0; i < ECAPI_BACKTRACE_DEEP; i++)
+        for (i = 0; i < ret; i++)
         {
             positionList[i] = &positionInfo[i * ECAPI_POSITION_LEN];
+            snprintf(positionList[i], ECAPI_POSITION_LEN,
+                "%08llX", (long long unsigned int)addrList[i]);
         }
 
         /* 减去系统内存地址偏移量,得到函数在elf文件中的实际地址(不是物理地址) */
-        if (ecapi_parse_proc_self_maps(addrList, positionList, ECAPI_POSITION_LEN) != 0)
+        if (ecapi_parse_proc_self_maps(addrList, ret, positionList, ECAPI_POSITION_LEN) != 0)
         {
-            perror("ecapi_parse_proc_self_maps failed \r\n");
+            fprintf(stderr, "ecapi_parse_proc_self_maps failed \r\n");
             ret = 0;
         }
     }
     else
-        perror("backtrace failed \r\n");
+        fprintf(stderr, "backtrace failed \r\n");
 
 #if 0
     char **strings = backtrace_symbols(pointList, ret);
-    printf("backtrace_symbols/%p \r\n", strings);
     if (strings) {
         for (i = 0; i < ret; i++)
             printf("%dth: %s \n", i, strings[i]);
@@ -95,7 +105,7 @@ static void ecapi_signal(int sig)
         g_ecapi_callback(sig, (char**)positionList, ret, g_ecapi_priv);
 }
 
-static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, int positionLen)
+static int ecapi_parse_proc_self_maps(uint64_t* addrList, int addrListLen, char** positionList, int positionLen)
 {
     int i, ret;
 
@@ -103,9 +113,10 @@ static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, i
     char line[1024] = {0};
     uint64_t addrBegin = 0;
     uint64_t addrEnd = 0;
-    uint64_t addrSize = 0;
+    uint64_t addrOffset = 0;
     char temp[1024];
     char file[1024] = {0};
+    Elf64_Ehdr ehdr64 = {0};
 
     int hitCount = 0;
     char hitArray[ECAPI_BACKTRACE_DEEP] = {0};
@@ -122,15 +133,21 @@ static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, i
             (long long unsigned int*)&addrBegin,
             (long long unsigned int*)&addrEnd,
             temp, /* premission */
-            (long long unsigned int*)&addrSize,
+            (long long unsigned int*)&addrOffset,
             temp, /* time */
             temp, /* unknown */
             temp); /* file */
 
-        // printf("%s // %08llX - %08llX - %08llX \r\n", line, addrBegin, addrEnd, addrSize);
+        // printf("%s // %08llX - %08llX - %08llX \r\n", line, addrBegin, addrEnd, addrOffset);
+
+        if (addrEnd > addrBegin && addrOffset == 0)
+        {
+            if (addrEnd - addrBegin > sizeof(Elf64_Ehdr))
+                memcpy(&ehdr64, (void*)addrBegin, sizeof(Elf64_Ehdr));
+        }
 
         /* 比较地址范围,找到对应的addrBegin,减去该值再加上块大小即是目标地址 */
-        for (i = 0; i < ECAPI_BACKTRACE_DEEP; i++)
+        for (i = 0; i < addrListLen; i++)
         {
             /* already hit */
             if (hitArray[i])
@@ -140,7 +157,8 @@ static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, i
             if (addrList[i] >= addrBegin && addrList[i] < addrEnd)
             {
                 /* get the addr of file */
-                addrList[i] = addrList[i] - addrBegin + addrSize;
+                if (!_isSameLevel(addrBegin, ehdr64.e_entry))
+                    addrList[i] = addrList[i] - addrBegin + addrOffset;
 
                 /* flag set */
                 hitArray[i] = 1;
@@ -151,7 +169,36 @@ static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, i
                 {
                     strcpy(file, temp);
                     if (ecelf_load(file, &elf))
-                        continue;
+                    {
+                        // fprintf(stderr, "ecelf_load() file %s failed [%08llX - %08llX - %08llX]\r\n", file,
+                        //     (long long unsigned int)addrBegin,
+                        //     (long long unsigned int)addrEnd,
+                        //     (long long unsigned int)addrOffset);
+                            
+                        // printf("ehdr64: "
+                        //     "e_phoff/%08llX "
+                        //     "e_shoff/%08llX "
+                        //     "e_flags/%08lX "
+                        //     "e_ehsize/%04X "
+                        //     "e_phentsize/%04X "
+                        //     "e_phnum/%04X "
+                        //     "e_shentsize/%04X "
+                        //     "e_shnum/%04X "
+                        //     "e_shstrndx/%04X "
+                        //     "\r\n",
+                        //     ehdr64.e_phoff,
+                        //     ehdr64.e_shoff,
+                        //     ehdr64.e_flags,
+                        //     ehdr64.e_ehsize,
+                        //     ehdr64.e_phentsize,
+                        //     ehdr64.e_phnum,
+                        //     ehdr64.e_shentsize,
+                        //     ehdr64.e_shnum,
+                        //     ehdr64.e_shstrndx);
+
+                        if (ecelf_loadByMem((uint8_t*)addrBegin, &elf))
+                            continue;
+                    }
                 }
 
                 ret = ecapi_loadSymbolFromElfByAddr(&elf, addrList[i], positionList[i], positionLen);
@@ -162,7 +209,7 @@ static int ecapi_parse_proc_self_maps(uint64_t* addrList, char** positionList, i
         }
 
         /* 匹配完成,提前跑路 */
-        if (hitCount == ECAPI_BACKTRACE_DEEP)
+        if (hitCount == addrListLen)
             break;
     }
 
@@ -190,44 +237,16 @@ static int ecapi_loadSymbolFromElfByAddr(ECElf_Struct* elf, uint64_t addr, char*
     }
     else
     {
-        /* check .symtab */
-        if (elf->symtab && elf->strtab)
-        {
-            for (i = 0, up32 = (uint32_t*)elf->symtab; i < elf->symtabSize; i += sizeof(Elf64_Sym))
-            {
-                /* 名称有效 && 地址有效 */
-                if (up32[0] < elf->strtabSize && up32[2])
-                {
-                    /* 找地址相差最小那个 */
-                    if (addr >= up32[2] && addr < up32[2] + up32[4])
-                    {
-                        tarSymbol = ((const char*)elf->strtab) + up32[0];
-                        break;
-                    }
-                    /* 找地址相差最小那个 */
-                    else
-                    {
-                        addrErr = addr - up32[2];
-                        if (addrErr < addrErrMin)
-                        {
-                            addrErrMin = addrErr;
-                            tarSymbol = ((const char*)elf->strtab) + up32[0];
-                        }
-                    }
-                }
-                up32 += sizeof(Elf64_Sym) / 4;
-            }
-        }
         /* check .dynsym */
         if (elf->dynsym && elf->dynstr)
         {
             for (i = 0, up32 = (uint32_t*)elf->dynsym; i < elf->dynsymSize; i += sizeof(Elf64_Sym))
             {
                 /* 名称有效 && 地址有效 */
-                if (up32[0] < elf->dynstrSize && up32[2])
+                if (up32[0] < elf->dynstrSize && up32[2] && addr >= up32[2])
                 {
                     /* 直接地址范围匹配 */
-                    if (addr >= up32[2] && addr < up32[2] + up32[4])
+                    if (up32[4] > 0 && addr < up32[2] + up32[4])
                     {
                         tarSymbol = ((const char*)elf->dynstr) + up32[0];
                         break;
@@ -240,6 +259,34 @@ static int ecapi_loadSymbolFromElfByAddr(ECElf_Struct* elf, uint64_t addr, char*
                         {
                             addrErrMin = addrErr;
                             tarSymbol = ((const char*)elf->dynstr) + up32[0];
+                        }
+                    }
+                }
+                up32 += sizeof(Elf64_Sym) / 4;
+            }
+        }
+        /* check .symtab */
+        if (elf->symtab && elf->strtab)
+        {
+            for (i = 0, up32 = (uint32_t*)elf->symtab; i < elf->symtabSize; i += sizeof(Elf64_Sym))
+            {
+                /* 名称有效 && 地址有效 */
+                if (up32[0] < elf->strtabSize  && up32[2] && addr >= up32[2])
+                {
+                    /* 直接地址范围匹配 */
+                    if (up32[4] > 0 && addr < up32[2] + up32[4])
+                    {
+                        tarSymbol = ((const char*)elf->strtab) + up32[0];
+                        break;
+                    }
+                    /* 找地址相差最小那个 */
+                    else
+                    {
+                        addrErr = addr - up32[2];
+                        if (addrErr < addrErrMin)
+                        {
+                            addrErrMin = addrErr;
+                            tarSymbol = ((const char*)elf->strtab) + up32[0];
                         }
                     }
                 }
@@ -293,7 +340,7 @@ static void ecelf_release(ECElf_Struct* elf)
 static int ecelf_load(const char* file, ECElf_Struct* elf)
 {
     // Elf32_Ehdr ehdr32;
-    Elf32_Shdr* shdr32 = NULL;
+    // Elf32_Shdr* shdr32 = NULL;
 
     Elf64_Ehdr ehdr64;
     Elf64_Shdr* shdr64 = NULL;
@@ -325,27 +372,29 @@ static int ecelf_load(const char* file, ECElf_Struct* elf)
             /* 读 elf 头,取 section header 偏移量 */
             if (fread(&ehdr64, 1, sizeof(Elf64_Ehdr), fp) != sizeof(Elf64_Ehdr))
                 break;
-            
-            // printf("ehdr64: "
-            //     "e_phoff/%08llX "
-            //     "e_shoff/%08llX "
-            //     "e_flags/%08lX "
-            //     "e_ehsize/%04X "
-            //     "e_phentsize/%04X "
-            //     "e_phnum/%04X "
-            //     "e_shentsize/%04X "
-            //     "e_shnum/%04X "
-            //     "e_shstrndx/%04X "
-            //     "\r\n",
-            //     ehdr64.e_phoff,
-            //     ehdr64.e_shoff,
-            //     ehdr64.e_flags,
-            //     ehdr64.e_ehsize,
-            //     ehdr64.e_phentsize,
-            //     ehdr64.e_phnum,
-            //     ehdr64.e_shentsize,
-            //     ehdr64.e_shnum,
-            //     ehdr64.e_shstrndx);
+
+#if 0
+            printf("ehdr64: "
+                "e_phoff/%08llX "
+                "e_shoff/%08llX "
+                "e_flags/%08lX "
+                "e_ehsize/%04X "
+                "e_phentsize/%04X "
+                "e_phnum/%04X "
+                "e_shentsize/%04X "
+                "e_shnum/%04X "
+                "e_shstrndx/%04X "
+                "\r\n",
+                ehdr64.e_phoff,
+                ehdr64.e_shoff,
+                ehdr64.e_flags,
+                ehdr64.e_ehsize,
+                ehdr64.e_phentsize,
+                ehdr64.e_phnum,
+                ehdr64.e_shentsize,
+                ehdr64.e_shnum,
+                ehdr64.e_shstrndx);
+#endif
 
             /* 没有 section header name table 则无法匹配 .strtab 和 .symtab */
             if (ehdr64.e_shstrndx == SHN_XINDEX || ehdr64.e_shstrndx >= ehdr64.e_shnum)
@@ -374,23 +423,25 @@ static int ecelf_load(const char* file, ECElf_Struct* elf)
             {
                 sectionName = shstrtab + shdr64[i].sh_name;
 
-                // printf("shdr64: %dth, "
-                //     "sh_name/%08llX "
-                //     "sh_type/%08llX "
-                //     "sh_flags/%08llX "
-                //     "sh_addr/%08llX "
-                //     "sh_offset/%08llX "
-                //     "sh_size/%08llX "
-                //     "%s "
-                //     "\r\n",
-                //     i,
-                //     shdr64[i].sh_name,
-                //     shdr64[i].sh_type,
-                //     shdr64[i].sh_flags,
-                //     shdr64[i].sh_addr,
-                //     shdr64[i].sh_offset,
-                //     shdr64[i].sh_size,
-                //     sectionName);
+#if 0
+                printf("shdr64: %dth, "
+                    "sh_name/%08llX "
+                    "sh_type/%08llX "
+                    "sh_flags/%08llX "
+                    "sh_addr/%08llX "
+                    "sh_offset/%08llX "
+                    "sh_size/%08llX "
+                    "%s "
+                    "\r\n",
+                    i,
+                    shdr64[i].sh_name,
+                    shdr64[i].sh_type,
+                    shdr64[i].sh_flags,
+                    shdr64[i].sh_addr,
+                    shdr64[i].sh_offset,
+                    shdr64[i].sh_size,
+                    sectionName);
+#endif
                 
                 /* 匹配目标 section */
                 if (shdr64[i].sh_type == SHT_SYMTAB &&
@@ -434,35 +485,37 @@ static int ecelf_load(const char* file, ECElf_Struct* elf)
                 if (fread(*tarMem, 1, *tarSize, fp) != *tarSize)
                     break;
 
-                // if (!strcmp((const char*)".symtab", sectionName))
-                // {
-                //     uint32_t j, k;
-                //     uint32_t* up32 = (uint32_t*)(*tarMem);
-                //     for (j = 0, k = 0; j < *tarSize; j += sizeof(Elf64_Sym), k++)
-                //     {
-                //         printf("sym64: %dth, "
-                //             "st_name/%08X "
-                //             "st_value/%08X "
-                //             "st_size/%08X "
-                //             "\r\n",
-                //             k,
-                //             up32[0],
-                //             up32[2],
-                //             up32[4]);
-                //         up32 += sizeof(Elf64_Sym) / 4;
-                //     }
-                // }
-                // else if (!strcmp((const char*)".dynstr", sectionName))
-                // {
-                //     uint32_t j;
-                //     char* str = NULL;
-                //     for (j = 0; j < *tarSize; j += strlen(str) + 1)
-                //     {
-                //         str = (char*)(*tarMem) + j;
-                //         if (*str)
-                //             printf("strtab: offset/%08X %s \r\n", j, str);
-                //     }
-                // }
+#if 0
+                if (!strcmp((const char*)".symtab", sectionName))
+                {
+                    uint32_t j, k;
+                    uint32_t* up32 = (uint32_t*)(*tarMem);
+                    for (j = 0, k = 0; j < *tarSize; j += sizeof(Elf64_Sym), k++)
+                    {
+                        printf("sym64: %dth, "
+                            "st_name/%08X "
+                            "st_value/%08X "
+                            "st_size/%08X "
+                            "\r\n",
+                            k,
+                            up32[0],
+                            up32[2],
+                            up32[4]);
+                        up32 += sizeof(Elf64_Sym) / 4;
+                    }
+                }
+                else if (!strcmp((const char*)".strtab", sectionName))
+                {
+                    uint32_t j;
+                    char* str = NULL;
+                    for (j = 0; j < *tarSize; j += strlen(str) + 1)
+                    {
+                        str = (char*)(*tarMem) + j;
+                        if (*str)
+                            printf("strtab: offset/%08X %s \r\n", j, str);
+                    }
+                }
+#endif
             }
         }
 
@@ -472,11 +525,104 @@ static int ecelf_load(const char* file, ECElf_Struct* elf)
     if (shstrtab)
         free(shstrtab);
 
-    if (shdr32)
-        free(shdr32);
+    // if (shdr32)
+    //     free(shdr32);
     if (shdr64)
         free(shdr64);
 
     fclose(fp);
+    return ret;
+}
+
+static int ecelf_loadByMem(uint8_t* addr, ECElf_Struct* elf)
+{
+    // Elf32_Ehdr ehdr32;
+    // Elf32_Shdr* shdr32 = NULL;
+
+    Elf64_Ehdr ehdr64;
+    Elf64_Shdr* shdr64 = NULL;
+
+    uint32_t shstrtabSize = 0;
+    char* shstrtab = NULL;
+    char* sectionName = NULL;
+
+    uint32_t* tarSize;
+    void** tarMem;
+
+    uint32_t i;
+    int ret = -1;
+
+    do
+    {
+        if (sizeof(void*) == 4)
+        {
+            ;
+        }
+        else
+        {
+            /* 读 elf 头,取 section header 偏移量 */
+            memcpy(&ehdr64, addr, sizeof(Elf64_Ehdr));
+
+            /* 没有 section header name table 则无法匹配 .strtab 和 .symtab */
+            if (ehdr64.e_shstrndx == SHN_XINDEX || ehdr64.e_shstrndx >= ehdr64.e_shnum)
+                break;
+            /* 理论应该一致 */
+            if (ehdr64.e_shentsize != sizeof(Elf64_Shdr))
+                break;
+
+            /* 全量读取 section header 内存 */
+            shdr64 = (Elf64_Shdr*)(addr + ehdr64.e_shoff);
+
+            /* 全量读取 shstrtab */
+            shstrtabSize = shdr64[ehdr64.e_shstrndx].sh_size;
+            shstrtab = (char*)(addr + shdr64[ehdr64.e_shstrndx].sh_offset);
+
+            /* 遍历 section header */
+            for (i = 0; i < ehdr64.e_shnum; i++)
+            {
+                sectionName = shstrtab + shdr64[i].sh_name;
+                
+                /* 匹配目标 section */
+                if (shdr64[i].sh_type == SHT_SYMTAB &&
+                    shdr64[i].sh_name < shstrtabSize &&
+                    !strcmp((const char*)".symtab", sectionName))
+                {
+                    tarSize = &elf->symtabSize;
+                    tarMem = &elf->symtab;
+                }
+                else if (shdr64[i].sh_type == SHT_STRTAB &&
+                    shdr64[i].sh_name < shstrtabSize &&
+                    !strcmp((const char*)".strtab", sectionName))
+                {
+                    tarSize = &elf->strtabSize;
+                    tarMem = &elf->strtab;
+                }
+                else if (shdr64[i].sh_type == SHT_DYNSYM &&
+                    shdr64[i].sh_name < shstrtabSize &&
+                    !strcmp((const char*)".dynsym", sectionName))
+                {
+                    tarSize = &elf->dynsymSize;
+                    tarMem = &elf->dynsym;
+                }
+                else if (shdr64[i].sh_type == SHT_STRTAB &&
+                    shdr64[i].sh_name < shstrtabSize &&
+                    !strcmp((const char*)".dynstr", sectionName))
+                {
+                    tarSize = &elf->dynstrSize;
+                    tarMem = &elf->dynstr;
+                }
+                else
+                    continue;
+
+                /* section 全量读取 */
+                *tarSize = (uint32_t)shdr64[i].sh_size;
+                *tarMem = calloc(*tarSize, 1);
+                memcpy(*tarMem, addr + shdr64[i].sh_offset, *tarSize);
+            }
+        }
+
+        ret = 0;
+    } while (0);
+
     return ret;
 }
